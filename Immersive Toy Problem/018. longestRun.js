1232f55316ca864917b4ef9c023d4e29b030d8f4
iden <iden@idenui-MacBook-Pro.local>
2020-09-07 23:57:46 +0900

    add the problem solved

--- Immersive Toy Problem/018. longestRun.js
+++ Immersive Toy Problem/018. longestRun.js
@@ -1,12 +1,24 @@
-const longestRun = function(string) {
-  // TODO: 여기에 코드를 작성합니다.
+const longestRun = function (string) {
+  let result = [0, 0];
+  let temp = [0, 0];
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] !== string[i - 1]) {
+      if (temp[1] - temp[0] > result[1] - result[0]) {
+        result = temp;
+      }
+      temp = [i, i];
+    } else {
+      temp[1] = i;
+    }
+  }
+  return result;
 };
 
 // 여러분들을 위해 무작위 문자열 생성기를 제공해 드리겠습니다!
 // (해답을 작성하는 데 꼭 필요하진 않지만, 테스트할 때 유용할 것입니다)
-var randomString = function(len) {
-  var text = '';
-  var possible = 'abcdefghijklmnopqrstuvwxyz';
+var randomString = function (len) {
+  var text = "";
+  var possible = "abcdefghijklmnopqrstuvwxyz";
 
   for (var i = 0; i < len; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -15,6 +27,6 @@ var randomString = function(len) {
   return text;
 };
 
-longestRun("abbbcc") // [1, 3]
-longestRun("aabbc")  // [0, 1]
-longestRun("abcd")   // [0, 0]
+longestRun("abbbcc"); // [1, 3]
+longestRun("aabbc"); // [0, 1]
+longestRun("abcd"); // [0, 0]
