332b406dce752be5ca7305ee723e9647c686d1f2
Iden Han <idenhan129@gmail.com>
2020-11-23 06:58:59 +0900

    Add the solved problem

--- Programmers/Coding Test/소수찾기.js
+++ Programmers/Coding Test/소수찾기.js
@@ -1,7 +1,37 @@
 function solution(numbers) {
-  var answer = 0;
+  let answer = 0;
+  let n = numbers.split('');
+  let nums = new Set();
+  permutation(n,'');
+  function permutation(a, s) {
+    if (s.length > 0) {
+      if (nums.has(Number(s))=== false) {
+        nums.add(Number(s));
+        if (chkPrime(Number(s))) {
+          answer++;
+        }
+      }
+    }
+    if (a.length > 0) {
+      for (let i = 0; i< a.length; i++) {
+        let t = a.slice(0)
+        t.splice(i,1);
+        console.log(t,s + a[i]);
+        permutation(t,s + a[i]);
+      }
+    }
+  }
+  function chkPrime(num) {
+    if (num < 2) return false;
+    if (num === 2) return true;
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num%i===0) return false;
+      }
+    return true;
+  }
   return answer;
 }
+
 
 /*
 문제 설명
