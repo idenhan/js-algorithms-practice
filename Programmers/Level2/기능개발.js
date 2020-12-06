320ecf1180c90076e1917e44474d21abd2a98e21
Iden Han <idenhan129@gmail.com>
2020-12-06 16:56:38 +0900

    Add the solved problem

--- Programmers/Level2/기능개발.js
+++ Programmers/Level2/기능개발.js
@@ -1,6 +1,26 @@
 function solution(progresses, speeds) {
-    var answer = [];
-    return answer;
+  let answer = [];
+  
+  while (speeds.length > 0) {
+    for (let i = 0; i < speeds.length; i++) {
+      if (progresses[i] < 100) {
+        progresses[i] += speeds[i];
+      }
+    }
+
+    let deployCount = 0;
+    while (progresses[0] >= 100) {
+      progresses.shift();
+      speeds.shift();
+      deployCount++;
+    }
+
+    if (deployCount > 0) {
+      answer.push(deployCount);
+    }
+  }
+  
+  return answer;
 }
 
 /*
