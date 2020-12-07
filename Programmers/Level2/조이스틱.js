9db9293d5467b5b4679f1b3038397f0bc8743631
Iden Han <idenhan129@gmail.com>
2020-12-08 00:38:36 +0900

    Add the solved problem

--- Programmers/Level2/조이스틱.js
+++ Programmers/Level2/조이스틱.js
@@ -1,6 +1,29 @@
 function solution(name) {
-  let answer = 0;
-  return answer;
+  const arr = [0];
+
+  const answer = [...name].reduce((answer, s, i) => {
+    if (s === "A") {
+      if (name[i-1] !== "A") arr.push(continuous(name.substring(i)) - (i-1));
+      return answer + 1;
+    }
+    return answer + ascii(name, i) + 1;
+  }, 0);
+
+  return answer - Math.max(...arr) - 1;
+}
+
+function ascii(name, i) {
+  const num = name.charCodeAt(i);
+  return (num > 78) ? 91 - num : num - 65;
+}
+
+function continuous(name) {
+  let repeat = 0;
+  for (let i = 0; i < name.length; i++) {
+    if (name[i] !== "A") break;
+    repeat++;
+  }
+  return repeat;
 }
 
 /*
