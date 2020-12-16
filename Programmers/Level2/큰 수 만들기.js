f5316bee726f71493c255da6c69bf240856600fc
Iden Han <idenhan129@gmail.com>
2020-12-16 22:34:10 +0900

    Add the solved problem

--- Programmers/Level2/큰 수 만들기.js
+++ Programmers/Level2/큰 수 만들기.js
@@ -1,5 +1,17 @@
 function solution(number, k) {
-  let answer = '';
+  let stack = [];
+  for (let i = 0; i < number.length; i++) {
+    let now = number[i];
+
+    while(k > 0 && stack[stack.length - 1] < now) {
+      stack.pop();
+      k--;
+    }
+    stack.push(now);
+  }
+
+  stack.splice(stack.length - k, k);
+  let answer = stack.join('');
   return answer;
 }
 
